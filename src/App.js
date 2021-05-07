b8c87ce49a1a33f82050b5509ea4db7e614d3963
David Dobbins <david.dobbins@nerdery.com>
2021-05-07 16:52:38 -0400

    adding more content

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import resumeDataJson from "./resumeData.json";
 
 export default function App() {
   return (
+  
     <div className="App">
       <Header data={resumeDataJson.main} />
       <About data={resumeDataJson.main} />
