d67bf1059912742f3ba02c2e90c2d9213cd01ccc
David Dobbins <david.dobbins@nerdery.com>
2021-05-07 14:45:04 -0400

    update the app imports

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 // css
 import "./App.css";
 // components
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Testimonials from "./Components/Testimonials";
-import Portfolio from "./Components/Portfolio";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Testimonials from "./components/Testimonials";
+import Portfolio from "./components/Portfolio";
 // data
 import resumeDataJson from "./resumeData.json";
 
