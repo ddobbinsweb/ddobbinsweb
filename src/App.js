03e256bce424c162f041924da6d6d9597cdf41f5
David Dobbins <ddobbinsweb@gmail.com>
2022-10-02 22:29:27 -0400

    Move nav outside of header

--- src/App.js
+++ src/App.js
@@ -2,46 +2,49 @@ import React from "react";
 // css
 import "./App.css";
 // components
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import About from "./components/About";
-import Resume from "./components/Resume";
 import Contact from "./components/Contact";
-import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 // data
 import resumeData from "./resumeData.json";
 
 const App = () => {
   const { main, resume, portfolio } = resumeData;
+
   return (
-    <div className="App">
-      <Header
-        name={main.name}
-        occupation={main.occupation}
-        description={main.description}
-        city={main.address.city}
-        social={main.social}
-      />
-      <About
-        name={main.name}
-        image={main.image}
-        bio={main.bio}
-        address={main.address}
-        email={main.email}
-        resumedownload={main.resumedownload}
-      />
-      <Resume
-        skillmessage={resume.skillmessage}
-        education={resume.education}
-        work={resume.work}
-        skills={resume.skills}
-        certifications={resume.certifications}
-      />
-      <Portfolio portfolio={portfolio.projects} />
-      {/* <Testimonials testimonies={testimonials.testimonials} /> */}
-      <Contact email={main.email} />
-      <Footer name={main.name} social={main.social} />
+    <div>
+      <div className="App">
+      <Nav />
+        <Header
+          name={main.name}
+          occupation={main.occupation}
+          description={main.description}
+          city={main.address.city}
+          social={main.social}
+        />
+        <About
+          name={main.name}
+          image={main.image}
+          bio={main.bio}
+          address={main.address}
+          email={main.email}
+          resumedownload={main.resumedownload}
+        />
+        <Resume
+          skillmessage={resume.skillmessage}
+          education={resume.education}
+          work={resume.work}
+          skills={resume.skills}
+          certifications={resume.certifications}
+        />
+        <Portfolio portfolio={portfolio.projects} />
+        <Contact email={main.email} />
+        <Footer name={main.name} social={main.social} />
+      </div>
     </div>
   );
 };
